08197649924daf3f83cfc6ca31e35bfe10d0adff
Ido Kleinman <idokleinman@gmail.com>
2016-02-24 21:04:10 -0800

    making the example app run SDK functions in a logical order to demo SDK usage

--- SparkSetupExample-Swift/ViewController.swift
+++ SparkSetupExample-Swift/ViewController.swift
@@ -86,86 +86,162 @@ class ViewController: UIViewController, SparkSetupMainControllerDelegate {
         }
     }
     
+    @IBAction func runSDKFuncs(sender: AnyObject) {
+        self.example()
+    }
+    
     func example()
     {
-        // logging in
-        if SparkCloud.sharedInstance().loggedInUsername == nil
-        {
-            SparkCloud.sharedInstance().loginWithUser("ido@spark.io", password: "test123") { (error:NSError?) -> Void in
+        let loginGroup : dispatch_group_t = dispatch_group_create()
+        let deviceGroup : dispatch_group_t = dispatch_group_create()
+        let priority = DISPATCH_QUEUE_PRIORITY_DEFAULT
+        let deviceName = "turtle_gerbil"
+        let functionName = "testFunc"
+        let variableName = "testVar"
+        var myPhoton : SparkDevice? = nil
+        
+        dispatch_async(dispatch_get_global_queue(priority, 0)) {
+            // logging in
+            dispatch_group_enter(loginGroup);
+            if SparkCloud.sharedInstance().loggedInUsername == nil
+            {
+                let keys = SparksetupexampleswiftKeys()
+                SparkCloud.sharedInstance().loginWithUser(keys.particleUsername(), password: keys.particlePassword()) { (error:NSError?) -> Void in
+                    if let _=error
+                    {
+                        print("Wrong credentials or no internet connectivity, please try again")
+                    }
+                    else
+                    {
+                        print("Logged in with user "+keys.particleUsername())
+                        dispatch_group_leave(loginGroup)
+                    }
+                }
+            } else {
+                print("Already logged in with user "+SparkCloud.sharedInstance().loggedInUsername!)
+                dispatch_group_leave(loginGroup)
+            }
+        }
+        
+        dispatch_async(dispatch_get_global_queue(priority, 0)) {
+            // logging in
+            dispatch_group_wait(loginGroup, DISPATCH_TIME_FOREVER)
+            dispatch_group_enter(deviceGroup);
+            
+            // get specific device by name:
+            SparkCloud.sharedInstance().getDevices { (sparkDevices:[AnyObject]?, error:NSError?) -> Void in
                 if let _=error
                 {
-                    print("Wrong credentials or no internet connectivity, please try again")
+                    print("Check your internet connectivity")
                 }
                 else
                 {
-                    print("Logged in")
+                    if let devices = sparkDevices as? [SparkDevice]
+                    {
+                        for device in devices
+                        {
+                            if device.name == deviceName
+                            {
+                                print("found a device with name "+deviceName+" in your account")
+                                myPhoton = device
+                                dispatch_group_leave(deviceGroup)
+                            }
+                            
+                        }
+                        if (myPhoton == nil)
+                        {
+                            print("device with name "+deviceName+" was not found in your account")
+                        }
+                    }
                 }
             }
         }
         
-        // get specific device by name:
-        var myPhoton : SparkDevice? = nil
-        SparkCloud.sharedInstance().getDevices { (sparkDevices:[AnyObject]?, error:NSError?) -> Void in
-            if let _=error
-            {
-                print("Check your internet connectivity")
-            }
-            else
-            {
-                if let devices = sparkDevices as? [SparkDevice]
-                {
-                    for device in devices
-                    {
-                        if device.name == "turtle_gerbil"
-                        {
-                            myPhoton = device
-                        }
-                        
-                    }
-                    if (myPhoton != nil)
-                    {
-                        print("subscribing to events...");
-                        myPhoton!.subscribeToEventsWithPrefix("test", handler: { (event: SparkEvent?, error:NSError?) -> Void in
-                            print(event!.event);
-                        });
-                        
-                    }
+        
+        dispatch_async(dispatch_get_global_queue(priority, 0)) {
+            // logging in
+            dispatch_group_wait(deviceGroup, DISPATCH_TIME_FOREVER)
+            dispatch_group_enter(deviceGroup);
+
+            print("subscribing to event...");
+            var gotFirstEvent : Bool = false
+            myPhoton!.subscribeToEventsWithPrefix("test", handler: { (event: SparkEvent?, error:NSError?) -> Void in
+                if (!gotFirstEvent) {
+                    print("Got first event: "+event!.event)
+                    gotFirstEvent = true
+                    dispatch_group_leave(deviceGroup)
+                } else {
+                    print("Got event: "+event!.event)
+                }
+            });
+        }
+        
+        
+        // calling a function
+        dispatch_async(dispatch_get_global_queue(priority, 0)) {
+            // logging in
+            dispatch_group_wait(deviceGroup, DISPATCH_TIME_FOREVER) // 5
+            dispatch_group_enter(deviceGroup);
+            
+            let funcArgs = ["D7",1]
+            myPhoton!.callFunction(functionName, withArguments: funcArgs) { (resultCode : NSNumber?, error : NSError?) -> Void in
+                if (error == nil) {
+                    print("Successfully called function "+functionName+" on device "+deviceName)
+                    dispatch_group_leave(deviceGroup)
+                } else {
+                    print("Failed to call function "+functionName+" on device "+deviceName)
                 }
             }
         }
         
         
         // reading a variable
-        myPhoton!.getVariable("temprature", completion: { (result:AnyObject?, error:NSError?) -> Void in
-            if let _=error
-            {
-                print("Failed reading temprature from device")
-            }
-            else
-            {
-                if let res = result as? Float
+        dispatch_async(dispatch_get_global_queue(priority, 0)) {
+            // logging in
+            dispatch_group_wait(deviceGroup, DISPATCH_TIME_FOREVER) // 5
+            dispatch_group_enter(deviceGroup);
+            
+            myPhoton!.getVariable(variableName, completion: { (result:AnyObject?, error:NSError?) -> Void in
+                if let _=error
                 {
-                    print("Room temprature is \(res) degrees")
+                    print("Failed reading variable "+variableName+" from device")
                 }
-            }
-        })
-        
-        
-        // calling a function
-        let funcArgs = ["D7",1]
-        myPhoton!.callFunction("digitalwrite", withArguments: funcArgs) { (resultCode : NSNumber?, error : NSError?) -> Void in
-            if (error == nil) {
-                print("LED on D7 successfully turned on")
-            }
+                else
+                {
+                    if let res = result as? Int
+                    {
+                        print("Variable "+variableName+" value is \(res)")
+                        dispatch_group_leave(deviceGroup)
+                    }
+                }
+            })
         }
         
+        
         // get device variables and functions
-        let myDeviceVariables : Dictionary? = myPhoton!.variables as Dictionary<String,String>
-        print("MyDevice first Variable is called \(myDeviceVariables!.keys.first) and is from type \(myDeviceVariables?.values.first)")
-
-        let myDeviceFunction = myPhoton!.functions
-        print("MyDevice first function is called \(myDeviceFunction.first)")
-
+        dispatch_async(dispatch_get_global_queue(priority, 0)) {
+            // logging in
+            dispatch_group_wait(deviceGroup, DISPATCH_TIME_FOREVER) // 5
+            dispatch_group_enter(deviceGroup);
+            
+            let myDeviceVariables : Dictionary? = myPhoton!.variables as Dictionary<String,String>
+            print("MyDevice first Variable is called \(myDeviceVariables!.keys.first) and is from type \(myDeviceVariables?.values.first)")
+            
+            let myDeviceFunction = myPhoton!.functions
+            print("MyDevice first function is called \(myDeviceFunction.first)")
+            dispatch_group_leave(deviceGroup)
+        }
+        
+        // logout
+        dispatch_async(dispatch_get_global_queue(priority, 0)) {
+            // logging in
+            dispatch_group_wait(deviceGroup, DISPATCH_TIME_FOREVER) // 5
+            
+            SparkCloud.sharedInstance().logout()
+        }
+        
+        /*
+        
         // get a device instance by ID
         var myOtherDevice : SparkDevice? = nil
         SparkCloud.sharedInstance().getDevice("53fa73265066544b16208184", completion: { (device:SparkDevice?, error:NSError?) -> Void in
@@ -184,8 +260,8 @@ class ViewController: UIViewController, SparkSetupMainControllerDelegate {
             
         })
 
-        // logout
-        SparkCloud.sharedInstance().logout()
+        */
+
         
 
     }
